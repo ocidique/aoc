e1904740dea712be179723ba49a3916a8eddad9f
Juho Pekkala <juho.pekkala@valamis.com>
2020-12-03 10:17:06 +0200

    add day02 part two
    - also fix day01 formatting

--- 2020/day01.go
+++ 2020/day01.go
@@ -1,76 +1,77 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func check(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func linesInFile(fileName string) []string {
-  f, err := os.Open(fileName)
-  check(err)
-  scanner := bufio.NewScanner(f)
-  result := []string{}
+	f, err := os.Open(fileName)
+	check(err)
+	scanner := bufio.NewScanner(f)
+	result := []string{}
 
-  for scanner.Scan() {
-    line := scanner.Text()
-    result = append(result, line)
-  }
-  return result
+	for scanner.Scan() {
+		line := scanner.Text()
+		result = append(result, line)
+	}
+	f.Close()
+	return result
 }
 
 func strToInt(n string) int {
-  x, err := strconv.Atoi(n)
-  check(err)	
-  return x
+	x, err := strconv.Atoi(n)
+	check(err)
+	return x
 }
 
 func checkSum(sum int, array []int) bool {
-  result := 0
-  for _, v := range array {
-    result += v
-  }
-  if result == sum {
-    return true
-  }
-  return false
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	if result == sum {
+		return true
+	}
+	return false
 }
 
 func multiplyNumbers(array []int) int {
-  result := 1
-  for _,v := range array {
-    result = result * v
-  }
-  return result
+	result := 1
+	for _, v := range array {
+		result = result * v
+	}
+	return result
 }
 
 func main() {
-  inputs := linesInFile(`day01_input.txt`)
-  sum := 2020
-  hasAnswer := false
-  for _, line := range inputs {
-    for _, lineParaller := range inputs {
-      if checkSum(sum, []int{strToInt(line), strToInt(lineParaller)}) {
-        fmt.Printf("Answer: %v\n", multiplyNumbers([]int{strToInt(line), strToInt(lineParaller)}))
-        break
-      }
-      for _, lineParaller2 := range inputs {
-        if (checkSum(sum, []int{strToInt(line), strToInt(lineParaller), strToInt(lineParaller2)})) {
-          fmt.Printf("Answer b: %v\n", multiplyNumbers([]int{strToInt(line), strToInt(lineParaller), strToInt(lineParaller2)}))
-          hasAnswer = true
-          break
-        }
-      }
-    }
-    if hasAnswer {
-      break
-    }
-  }
+	inputs := linesInFile(`day01_input.txt`)
+	sum := 2020
+	hasAnswer := false
+	for _, line := range inputs {
+		for _, lineParaller := range inputs {
+			if checkSum(sum, []int{strToInt(line), strToInt(lineParaller)}) {
+				fmt.Printf("Answer: %v\n", multiplyNumbers([]int{strToInt(line), strToInt(lineParaller)}))
+				break
+			}
+			for _, lineParaller2 := range inputs {
+				if checkSum(sum, []int{strToInt(line), strToInt(lineParaller), strToInt(lineParaller2)}) {
+					fmt.Printf("Answer b: %v\n", multiplyNumbers([]int{strToInt(line), strToInt(lineParaller), strToInt(lineParaller2)}))
+					hasAnswer = true
+					break
+				}
+			}
+		}
+		if hasAnswer {
+			break
+		}
+	}
 }
--- 2020/day02.go
+++ 2020/day02.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 
 	"./utils"
 )
 
-func getParams(regEx, url string) (paramsMap map[string]string) {
+func getParams(regEx, input string) (paramsMap map[string]string) {
 
 	var compRegEx = regexp.MustCompile(regEx)
-	match := compRegEx.FindStringSubmatch(url)
+	match := compRegEx.FindStringSubmatch(input)
 
 	paramsMap = make(map[string]string)
 	for i, name := range compRegEx.SubexpNames() {
@@ -22,22 +23,31 @@ func getParams(regEx, url string) (paramsMap map[string]string) {
 	return
 }
 
-func countStringOccurences(str, key string) int {
-	return strings.Count(str, key)
-}
-
 func main() {
+	start := time.Now()
 	inputs := utils.LinesInFile("day02_input.txt")
 	regex := `(?P<min>\d+)-(?P<max>\d+) (?P<key>\w): (?P<pwd>\w+)`
 	validPwds := 0
+	validPwdsPartTwo := 0
 
 	for _, input := range inputs {
 		line := getParams(regex, input)
-		if strings.Count(line["pwd"], line["key"]) >= utils.StrToInt(line["min"]) &&
-			strings.Count(line["pwd"], line["key"]) <= utils.StrToInt(line["max"]) {
+		min := utils.StrToInt(line["min"])
+		max := utils.StrToInt(line["max"])
+
+		// part one check if key is between min max values
+		if strings.Count(line["pwd"], line["key"]) >= min &&
+			strings.Count(line["pwd"], line["key"]) <= max {
 			validPwds++
+		}
+
+		// part two check if key is either min-1 or max-1 index position, but not on both
+		if (string(line["pwd"][min-1]) == line["key"] && !(string(line["pwd"][max-1]) == line["key"])) || (!(string(line["pwd"][min-1]) == line["key"]) && string(line["pwd"][max-1]) == line["key"]) {
+			validPwdsPartTwo++
 		}
 	}
 
 	fmt.Println("Valid passwords: ", validPwds)
+	fmt.Println("Valid passwords part two: ", validPwdsPartTwo)
+	fmt.Println("Execution time: ", time.Since(start))
 }
