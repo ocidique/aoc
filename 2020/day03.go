90729bb50a5f40557c8b9c9d2cc304c860ca24f5
Juho Pekkala <juho.pekkala@gmail.com>
2020-12-04 22:26:04 +0200

    add day03 part two

--- 2020/day03.go
+++ 2020/day03.go
@@ -12,19 +12,41 @@ func main() {
 	lines := utils.LinesInFile("day03_input.txt")
 	droidWeAreLookingFor := "#"
 	droidCount := 0
-	xIncrement := 3
 	curPosX := 0
 	boundary := 0
+	droidsTotal := 1
 
-	for index, line := range lines {
-		boundary = len(line)
-		curPosX = index * xIncrement % boundary
-
-		if string(line[curPosX]) == droidWeAreLookingFor {
-			droidCount++
-		}
+	// using anonymous struct
+	slopes := []struct {
+		x, y int
+	}{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
 	}
 
-	fmt.Println("Droids found: ", droidCount)
+	for _, slope := range slopes {
+		droidCount = 0
+		for index, line := range lines {
+			boundary = len(line)
+
+			// skip line in case of index and yIncrement remainder > 0
+			if index%slope.y > 0 {
+				continue
+			}
+
+			curPosX = index * slope.x / slope.y % boundary
+			if string(line[curPosX]) == droidWeAreLookingFor {
+				droidCount++
+			}
+		}
+		fmt.Println("Droids on a slope: ", slope, droidCount)
+		droidsTotal *= droidCount
+
+	}
+
+	fmt.Println("Droids total: ", droidsTotal)
 	fmt.Println("Execution time: ", time.Since(start))
 }
