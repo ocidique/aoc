25ab8fc7f98b4eb3759da643fa5012d32fbb6cca
Juho Pekkala <juho.pekkala@gmail.com>
2020-12-07 23:18:31 +0200

    add day05 part two

--- 2020/day05.go
+++ 2020/day05.go
@@ -8,7 +8,7 @@ import (
 	"./utils"
 )
 
-func bsearch(ticket string, i int, min float64, max float64) float64 {
+func bSearch(ticket string, i int, min float64, max float64) float64 {
 	for _, chunk := range ticket {
 		if chunk == 'F' || chunk == 'L' {
 			max -= math.Ceil((max - min) / 2)
@@ -19,24 +19,39 @@ func bsearch(ticket string, i int, min float64, max float64) float64 {
 			return max
 		}
 	}
-	return bsearch(ticket, i+1, min, max)
+	return bSearch(ticket, i+1, min, max)
+}
+
+func hasTicket(seatID int, takenSeats []int) bool {
+	for _, ticketID := range takenSeats {
+		if seatID == ticketID {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	start := time.Now()
 	tickets := utils.LinesInFile("day05_input.txt")
 	maxSeatID := 0
+	takenSeats := []int{}
 
 	for _, ticket := range tickets {
-		row := bsearch(ticket[:7], 0, 0, 127)
-		col := bsearch(ticket[7:], 0, 0, 7)
+		row := bSearch(ticket[:7], 0, 0, 127)
+		col := bSearch(ticket[7:], 0, 0, 7)
 		seatID := int(row)*8 + int(col)
 		if maxSeatID < seatID {
 			maxSeatID = seatID
 		}
+		takenSeats = append(takenSeats, seatID)
 	}
+	fmt.Println("Part one, max seatID: ", maxSeatID)
 
-	// part one maxSeatID
-	fmt.Println("maxSeatID: ", maxSeatID)
+	for i := 0; i < len(takenSeats); i++ {
+		if x, y, z := hasTicket(i, takenSeats), hasTicket(i-1, takenSeats), hasTicket(i+1, takenSeats); !x && y && z {
+			fmt.Println("Part two, my seat: ", i)
+		}
+	}
 	fmt.Println("Execution time: ", time.Since(start))
 }
