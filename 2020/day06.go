983938c4f0e9382f1f68d838ca6bb31c874686c8
Juho Pekkala <juho.pekkala@valamis.com>
2020-12-08 17:48:18 +0200

    add day06 part two

--- 2020/day06.go
+++ 2020/day06.go
@@ -22,6 +22,10 @@ func contains(chars []string, char string) bool {
 	return false
 }
 
+func removeYesGroup(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
+}
+
 func main() {
 	start := time.Now()
 	// actually byte[] to string is easier to handle than string[] with some of these data sets
@@ -29,6 +33,7 @@ func main() {
 	utils.Check(err)
 	lines := removeEmptyLines(string(input))
 	total := 0
+	totalPartTwo := 0
 
 	for _, line := range lines {
 		line = strings.Replace(line, "\n", "", -1)
@@ -42,6 +47,23 @@ func main() {
 		total += len(chars)
 	}
 
+	for _, line := range lines {
+		split := strings.Split(line, "\n")
+		chars := split[0]
+		split = removeYesGroup(split, 0)
+		for _, s := range split {
+			for _, c := range chars {
+				chunk := string(c)
+				if !strings.Contains(s, chunk) {
+					chars = strings.Replace(chars, chunk, "", -1)
+				}
+			}
+
+		}
+		totalPartTwo += len(chars)
+	}
+
 	fmt.Println("Total: ", total)
+	fmt.Println("Total part two: ", totalPartTwo)
 	fmt.Println("Execution time: ", time.Since(start))
 }
